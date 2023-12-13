0673a031bda42dec07e8f5672e74d5fbc89bef41
Gabriel Sobral <gabriel.sobral1367@gmail.com>
2023-12-13 09:58:32 -0300

    delete: node server

--- server-node/prisma/migrations/20230726133638_adding_written_at_field_to_message_model/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `written_at` to the `Message` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE "Message" ADD COLUMN     "written_at" TIMESTAMP(3) NOT NULL;
--- server-node/src/infra/authentication/jwt/json-web-token.ts
+++
@@ -1,7 +0,0 @@
-export interface JWTPayload {
-  email: string;
-}
-
-export abstract class IJsonWebToken {
-  abstract sign: (payload: JWTPayload, expiresInSeconds: number) => string;
-}
