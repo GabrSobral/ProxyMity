4a6b2f3809acfb093d48ccbc94b6c704ef69931a
Gabriel Sobral <gabriel.sobral1367@gmail.com>
2024-01-16 00:04:37 -0300

    feat: adding melt-ui and profile drop down

--- svelte-app/src/lib/design-system/Button.svelte
+++ svelte-app/src/lib/design-system/Button.svelte
@@ -18,7 +18,7 @@
 	tabindex={tabIndex}
 	class={twMerge(
 		clsx(
-			'disabled:cursor-not-allowed disabled:opacity-40 min-h-[44px] border-2 border-solid h-fit w-fit whitespace-nowrap px-4 py-[0.375rem] rounded-md flex gap-2 items-center justify-center font-semibold hover:brightness-90 transition-all outline-none focus:outline-purple-500 focus:ring-0',
+			'disabled:cursor-not-allowed disabled:opacity-40 min-h-[38px] border-2 border-solid h-fit w-fit whitespace-nowrap px-4 py-[0.375rem] rounded-md flex gap-2 items-center justify-center font-semibold hover:brightness-90 transition-all outline-none focus:outline-purple-500 focus:ring-0',
 			{
 				'border-purple-500 text-white bg-gradient-to-r from-[#B809A6] border-0 to-[#1C64CE]': !isOutlined,
 				'border-gray-200 dark:border-gray-700 dark:text-gray-200 text-gray-700 bg-transparent': isOutlined,
