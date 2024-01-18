e5d8dd906987337aa59ce5e0f73df2a4da9559b5
Gabriel Sobral <gabriel.sobral1367@gmail.com>
2024-01-18 08:02:36 -0300

    feat: adding new notifications popober

--- svelte-app/src/lib/application/Header/NotificationButton.svelte
+++ svelte-app/src/lib/application/Header/NotificationButton.svelte
@@ -1,24 +1,94 @@
 <script lang="ts">
+   import * as Tabs from '$lib/components/ui/tabs';
+   import * as Avatar from '$lib/components/ui/avatar';
    import * as DropdownMenu from '$lib/components/ui/dropdown-menu';
 
-   import { Bell } from 'lucide-svelte';
+   import Text from '$lib/design-system/Text.svelte';
+
+   import { Bell, CheckCheck, Settings } from 'lucide-svelte';
+   import Button from '$lib/components/ui/button/button.svelte';
 </script>
 
 <DropdownMenu.Root>
-   <DropdownMenu.Trigger asChild>
-      <button type="button" class="flex p-3 rounded-md bg-black border border-gray-800" aria-label="Profile">
-         <span class="sr-only">Notifications</span>
-         <Bell size={24} class="text-white" />
-      </button>
+   <DropdownMenu.Trigger class="flex p-3 rounded-md bg-black border border-gray-800" aria-label="Profile">
+      <span class="sr-only">Notifications</span>
+      <Bell size={24} class="text-white" />
    </DropdownMenu.Trigger>
 
    <DropdownMenu.Content>
       <DropdownMenu.Group>
-         <DropdownMenu.Label>Notifications 17</DropdownMenu.Label>
+         <DropdownMenu.Label class="text-lg flex gap-4 w-full items-center">
+            Notifications 
+            <span class="font-semibold text-sm flex items-center justify-center min-w-[22px] min-h-[22px] max-w-[22px] max-h-[22px] dark:bg-white dark:text-black bg-black text-white rounded-full p-1">17</span>
+         </DropdownMenu.Label>
+
+         <Tabs.Root value="all-notifications">
+            <Tabs.List>
+               <Tabs.Trigger value="all-notifications">View all</Tabs.Trigger>
+               <Tabs.Trigger value="mention-notifications">Mentions</Tabs.Trigger>
+            </Tabs.List>
+
+            <Tabs.Content value="all-notifications">
+               <DropdownMenu.Item class="flex gap-4 max-w-[30rem] min-w-[25rem] overflow-hidden">
+                  <Avatar.Root>
+                     <Avatar.Image src="https://github.com/GabrSobral.png" alt="@shadcn" />
+                     <Avatar.Fallback>CN</Avatar.Fallback>
+                  </Avatar.Root>
+
+                  <div class="flex flex-col gap-2 w-full">
+                     <Text size="md" className="font-semibold">
+                        <span class="underline">Python</span> sent a new message
+                     </Text>
+
+                     <Text size="sm" className="text-gray-600">{new Date().toUTCString()}</Text>
+                  </div>
+               </DropdownMenu.Item>
+
+               <DropdownMenu.Item class="flex gap-4 max-w-[30rem] min-w-[25rem] overflow-hidden">
+                  <Avatar.Root>
+                     <Avatar.Image src="https://github.com/GabrSobral.png" alt="@shadcn" />
+                     <Avatar.Fallback>CN</Avatar.Fallback>
+                  </Avatar.Root>
+
+                  <div class="flex flex-col gap-2 w-full">
+                     <Text size="md" className="font-semibold">
+                        <span class="underline">Python</span> sent a new message
+                     </Text>
+
+                     <Text size="sm" className="text-gray-600">{new Date().toUTCString()}</Text>
+                  </div>
+               </DropdownMenu.Item>
+            </Tabs.Content>
+
+            <Tabs.Content value="mention-notifications">
+               <DropdownMenu.Item class="flex gap-4 max-w-[30rem] min-w-[25rem] overflow-hidden">
+                  <Avatar.Root>
+                     <Avatar.Image src="https://github.com/GabrSobral.png" alt="@shadcn" />
+                     <Avatar.Fallback>CN</Avatar.Fallback>
+                  </Avatar.Root>
+
+                  <div class="flex flex-col gap-2 w-full">
+                     <Text size="md" className="font-semibold">
+                        <span class="underline">Python</span> sent a new message
+                     </Text>
+
+                     <Text size="sm" className="text-gray-600">{new Date().toUTCString()}</Text>
+                  </div>
+               </DropdownMenu.Item>
+            </Tabs.Content>
+         </Tabs.Root>
 
          <DropdownMenu.Separator />
 
-         <DropdownMenu.Item>Notification 1</DropdownMenu.Item>
+         <div class="px-3 py-2 flex justify-between items-center gap-4">
+            <a href="#" title="Notification settings" aria-label="Notification settings">
+               <Settings class="dark:text-white text-black" />
+            </a>
+
+            <Button class="py-1 h-8">
+               <CheckCheck class="text-black" /> Mark all as read
+            </Button>
+         </div>
       </DropdownMenu.Group>
    </DropdownMenu.Content>
 </DropdownMenu.Root>
